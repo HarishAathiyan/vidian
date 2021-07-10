3469f85cd0a3e9e620b8eff4cd46f384290ea7c8
Harish Aathiyan <75715998+HarishAathiyan@users.noreply.github.com>
2021-07-10 15:28:08 +0530

    Update javasource.js

--- docs/web3DGraphics/javasource.js
+++ docs/web3DGraphics/javasource.js
@@ -1,86 +1,24 @@
-var container, stats;
+			const scene = new THREE.Scene();
+			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var camera, controls, scene, renderer;
+			const renderer = new THREE.WebGLRenderer();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( renderer.domElement );
 
-var cross;
+			const geometry = new THREE.BoxGeometry();
+			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+			const cube = new THREE.Mesh( geometry, material );
+			scene.add( cube );
 
-init();
-animate();
+			camera.position.z = 5;
 
-function init() {
+			const animate = function () {
+				requestAnimationFrame( animate );
 
-  camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
-  camera.position.z = 500;
+				cube.rotation.x += 0.01;
+				cube.rotation.y += 0.01;
 
-  controls = new THREE.OrbitControls( camera );
-  controls.addEventListener( 'change', render );
+				renderer.render( scene, camera );
+			};
 
-  scene = new THREE.Scene();
-  scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
-
-  // world
-
-  var geometry = new THREE.CylinderGeometry( 0, 10, 30, 4, 1 );
-  var material = new THREE.MeshLambertMaterial( { color:0xffffff, shading: THREE.FlatShading } );
-
-  for ( var i = 0; i < 500; i ++ ) {
-
-    var mesh = new THREE.Mesh( geometry, material );
-    mesh.position.x = ( Math.random() - 0.5 ) * 1000;
-    mesh.position.y = ( Math.random() - 0.5 ) * 1000;
-    mesh.position.z = ( Math.random() - 0.5 ) * 1000;
-    mesh.updateMatrix();
-    mesh.matrixAutoUpdate = false;
-    scene.add( mesh );
-
-  }
-
-
-  // lights
-
-  light = new THREE.DirectionalLight( 0xffffff );
-  light.position.set( 1, 1, 1 );
-  scene.add( light );
-
-  light = new THREE.DirectionalLight( 0x002288 );
-  light.position.set( -1, -1, -1 );
-  scene.add( light );
-
-  light = new THREE.AmbientLight( 0x222222 );
-  scene.add( light );
-
-
-  // renderer
-
-  renderer = new THREE.WebGLRenderer( { antialias: false } );
-  renderer.setClearColor( scene.fog.color, 1 );
-  renderer.setSize( window.innerWidth, window.innerHeight );
-
-  container = document.getElementById( 'container' );
-  container.appendChild( renderer.domElement );
-
-  window.addEventListener( 'resize', onWindowResize, false );
-
-}
-
-function onWindowResize() {
-
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize( window.innerWidth, window.innerHeight );
-
-  render();
-
-}
-
-function animate() {
-
-  requestAnimationFrame( animate );
-  controls.update();
-
-}
-
-function render() {
-  renderer.render( scene, camera );
-}
+			animate();
